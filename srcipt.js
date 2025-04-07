e27ece8a1f4945bd353c0164a9325b3ae70cdc7e
Arthur <155775801+Arthurbot02@users.noreply.github.com>
2025-04-07 16:53:14 -0300

    Add files via upload

---
+++ srcipt.js
@@ -0,0 +1,4 @@
+// Exemplo de interatividade (quando clicado no texto, aparece um alerta)
+document.querySelector(".content").addEventListener("click", function () {
+  alert("Você clicou na mensagem de amor!");
+});
