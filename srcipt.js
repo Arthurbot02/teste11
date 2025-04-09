ba18bee754fe2762fda548a1e318a94a9a812e9b
Arthur <155775801+Arthurbot02@users.noreply.github.com>
2025-04-09 19:48:02 -0300

    Delete srcipt.js

--- srcipt.js
+++
@@ -1,4 +0,0 @@
-// Exemplo de interatividade (quando clicado no texto, aparece um alerta)
-document.querySelector(".content").addEventListener("click", function () {
-  alert("Você clicou na mensagem de amor!");
-});
